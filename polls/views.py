3440f79483042e69bbaa0420e2cc21d312f03422
Kaustuv <Kaustuv-Karki>
2024-07-20 00:43:40 +0545

    Changes

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+from .models import Choice , Question 
+from django.urls import reverse
+from django.db.models import F
 
 
 from .models import Question
@@ -25,10 +29,25 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render (
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': "You didn't select a choice",
+            }
+        )
+    else:   
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}"
     return HttpResponse(response)
 
-
-def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
