3736a98178f5ae428a36973c4e24424b587895b3
Kaustuv <Kaustuv-Karki>
2024-07-20 01:21:03 +0545

    Created generics

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from .models import Choice , Question 
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 
 
 from .models import Question
@@ -48,6 +49,23 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 def results(request, question_id):
-    response = f"You're looking at the results of question {question_id}"
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
